3a67c1ebf2c006706fbc15940005dedec41f171c
Zach Schuster <ztschuster@gmail.com>
2020-04-06 15:07:19 -0400

    create circleAvatar with business card photo

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/bus_card_photo.jpg'),
+                minRadius: 75,
+                maxRadius: 100,
+              ),
+              Text(
+                'Zach Schuster',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
