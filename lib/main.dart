deb387d6c86cc086c83778653e5d2a21b4e14968
Zach Schuster <ztschuster@gmail.com>
2020-04-08 14:28:53 -0400

    finish business card app

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 backgroundImage: AssetImage('images/bus_card_photo.jpg'),
@@ -21,19 +22,66 @@ class MyApp extends StatelessWidget {
               Text(
                 'Zach Schuster',
                 style: TextStyle(
-                  fontFamily: 'SourceCodePro',
-                  color: Colors.white70,
-                  fontSize: 40.0
-                ),
+                    fontFamily: 'SourceCodePro',
+                    color: Colors.grey.shade900,
+                    fontSize: 40.0),
               ),
               Text(
-                'ML Engineer',
+                'ML ENGINEER',
                 style: TextStyle(
-                  fontFamily: 'SourceSansPro',
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.grey.shade900,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.75),
+              ),
+              SizedBox(
+                height: 20,
+                  width: 200,
+                child: Divider(
                   color: Colors.white70,
-                  fontSize: 35.0
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Card(
+                    color: Colors.teal.shade500,
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.white70,
+                        size: 25,
+                      ),
+                      title: Text(
+                        '123 456 7899',
+                        style: TextStyle(
+                          fontFamily: 'SourceCodePro',
+                          fontSize: 25,
+                        ),
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Card(
+                    color: Colors.teal.shade500,
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.alternate_email,
+                        color: Colors.white70,
+                        size: 25,
+                      ),
+                      title: Text(
+                        'ztschuster@gmail.com',
+                        style: TextStyle(
+                          fontFamily: 'SourceCodePro',
+                          fontSize: 22,
+                        ),
+                      ),
+                    )),
+              )
             ],
           ),
         ),
